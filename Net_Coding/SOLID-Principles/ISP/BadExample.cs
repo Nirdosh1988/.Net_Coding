8a5626cfd739407ba5abbbbc84a2d266c1dee2a2
ram <shashi.rambadan@gmail.com>
2026-06-28 12:21:00 +0530

    w.r.t : ISP added

---
+++ Net_Coding/SOLID-Principles/ISP/BadExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.ISP
+{
+    public interface IWorker
+    {
+        void Work();
+        void Eat();
+        void Sleep();
+    }
+    public class RobotBadExample : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot is working.");
+        }
+
+        public void Eat()
+        {
+            throw new NotImplementedException("Robot doesn't eat.");
+        }
+
+        public void Sleep()
+        {
+            throw new NotImplementedException("Robot doesn't sleep.");
+        }
+    }
+}
---
+++ Net_Coding/SOLID-Principles/ISP/GoodExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.ISP
+{
+    public interface IWork
+    {
+        void Work();
+    }
+
+    public interface IEat
+    {
+        void Eat();
+    }
+
+    public interface ISleep
+    {
+        void Sleep();
+    }
+    public class HumanGoodExample : IWork, IEat, ISleep
+    {
+        public void Work()
+        {
+            Console.WriteLine("Human is working.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Human is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Human is sleeping.");
+        }
+    }
+
+    public class RobotGoodExample : IWork
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot is working.");
+        }
+    }
+}
