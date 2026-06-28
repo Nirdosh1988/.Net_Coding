da6de2f8363ab0a88f00c22ac72bf9d7dbd7943d
ram <shashi.rambadan@gmail.com>
2026-06-28 10:37:59 +0530

    w.r.t: OCP updated

---
+++ Net_Coding/SOLID-Principles/OCP/BadExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.OCP
+{
+    public class PaymentServiceBadExample
+    {
+        public void ProcessPayment(string paymentType, decimal amount)
+        {
+            if (paymentType == "CreditCard")
+            {
+                // Process credit card payment
+                Console.WriteLine($"Processing credit card payment of {amount:C}");
+            }
+            else if (paymentType == "PayPal")
+            {
+                // Process PayPal payment
+                Console.WriteLine($"Processing PayPal payment of {amount:C}");
+            }
+            else if (paymentType == "BankTransfer")
+            {
+                // Process bank transfer payment
+                Console.WriteLine($"Processing bank transfer payment of {amount:C}");
+            }
+            else
+            {
+                throw new NotSupportedException($"Payment type '{paymentType}' is not supported.");
+            }
+        }
+
+    }
+
+    public class ExecutePaymenServiceBadExample
+    {
+        public void Execute()
+        {
+            PaymentServiceBadExample service = new PaymentServiceBadExample();
+            service.ProcessPayment("PayPal", 1500);
+        }
+    }
+}
---
+++ Net_Coding/SOLID-Principles/OCP/GoodExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.OCP
+{
+    public interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+    public class CreditCardPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid ₹{amount} using Credit Card.");
+        }
+    }
+    public class PayPalPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid ₹{amount} using PayPal.");
+        }
+    }
+    public class PaymentServiceGoodExample
+    {
+        public void ProcessPayment(IPayment payment, decimal amount)
+        {
+            payment.Pay(amount);
+        }
+    }
+    public class ExecutePaymenService
+    {
+        public void Execute()
+        {
+            PaymentServiceGoodExample service = new PaymentServiceGoodExample();
+            IPayment payment = new PayPalPayment();
+            service.ProcessPayment(payment, 1500);
+        }
+    }
+
+}
